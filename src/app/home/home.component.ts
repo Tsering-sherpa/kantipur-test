43e3b0dd753d6a38467f10e4b1c0f7909991a4d8
Tsering Sherpa <tseringws13@gmail.com>
2022-07-23 01:31:34 +0545

    feat: fetch all api response

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { takeUntil } from 'rxjs';
+import { UserProfileService } from '../user-profile.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  public allUserData: any[] = [];
 
-  constructor() { }
+  constructor(private profileService: UserProfileService) {}
 
   ngOnInit(): void {
+    this.profileService.getAllUserData().subscribe((res) => {
+      this.allUserData = res;
+    });
   }
 
+  public deleteUser(user: any): void {}
+
+  public addNewUser(): void {}
 }
