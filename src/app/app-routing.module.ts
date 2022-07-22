2ce89335e26326e71ffd2c58e0d7f11fb3aba319
Tsering Sherpa <tseringws13@gmail.com>
2022-07-23 00:39:26 +0545

    feat: add routing for components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddProfileRecordComponent } from './add-profile-record/add-profile-record.component';
+import { DetailProfileRecordComponent } from './detail-profile-record/detail-profile-record.component';
+import { EditProfileRecordComponent } from './edit-profile-record/edit-profile-record.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'view-profile/:userId', component: DetailProfileRecordComponent },
+  { path: 'add-profile', component: AddProfileRecordComponent },
+  { path: 'edit-profile/:userId', component: EditProfileRecordComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
