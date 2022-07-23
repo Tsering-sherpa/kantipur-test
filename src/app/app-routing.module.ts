759ac7ab0016aee312f4796e98a919830c64d4da
Tsering Sherpa <tseringws13@gmail.com>
2022-07-23 16:48:35 +0545

    feat: remove routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AddProfileRecordComponent } from './add-profile-record/add-profile-record.component';
 import { DetailProfileRecordComponent } from './detail-profile-record/detail-profile-record.component';
-import { EditProfileRecordComponent } from './edit-profile-record/edit-profile-record.component';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
@@ -10,7 +9,7 @@ const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'view-profile/:userId', component: DetailProfileRecordComponent },
   { path: 'add-profile', component: AddProfileRecordComponent },
-  { path: 'edit-profile/:userId', component: EditProfileRecordComponent },
+  { path: 'add-profile/:userId', component: AddProfileRecordComponent },
 ];
 
 @NgModule({
